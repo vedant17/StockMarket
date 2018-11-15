648379155bfa3ee6402de17b41b9fb8357afa3fc
Vedant <vedantbajaj17@gmail.com>
2018-11-15 09:12:58 +0530

    Third Commit

--- stockMarket/importSharesTradedvsDelivered.py
+++ stockMarket/importSharesTradedvsDelivered.py
@@ -1,9 +1,7 @@
 
 
 import pandas
-import matplotlib
 import matplotlib.pyplot as plt
-import seaborn
 import numpy
 
 data1 = pandas.read_csv("/home/vegeta/NIFTYDATA/01-01-2015-TO-01-03-2015TATAMOTORSALLN.csv")
@@ -24,15 +22,15 @@ fig, ax1 = plt.subplots()
 #
 color = 'tab:red'
 ax1.set_xlabel('time ')
-ax1.set_ylabel('deliveredRatio', color=color)
+ax1.set_ylabel('Delivered Ratio', color=color)
 ax1.plot(deliveredRatio, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 #
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 #
 color = 'tab:blue'
-ax2.set_ylabel('closing Price', color=color)  # we already handled the x-label with ax1
-ax2.plot(closingPrice, color=color)
+ax2.set_ylabel('Shares Traded', color=color)  # we already handled the x-label with ax1
+ax2.plot(sharesTraded, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
 
